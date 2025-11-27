aca2af36aba30836ea900086e3142cd3172b38b2
Vlad Lipskiy <eswcvlad@gmail.com>
2025-11-27 23:42:07 +0300

    Adds conversion of models to ONNX.
    Some tweaks have been made:
    * "feature" from the CRAFT output is ignored.
    * CRAFT output is reshaped back to BCHW.
    * Input height to the recognition models have been made static and was
      set to the default config value (64).
    
    Last one is important, as `AdaptiveAvgPool2d` is not converting properly
    to ONNX. So the easiest way to fix that was to just assume the height
    static and replace it with regular `AvgPool2d`.
    
    Conversion of the "tamil_g1" still causes some errors, so it is skipped.

--- easyocr/model/vgg_model.py
+++ easyocr/model/vgg_model.py
@@ -8,7 +8,8 @@ class Model(nn.Module):
         """ FeatureExtraction """
         self.FeatureExtraction = VGG_FeatureExtractor(input_channel, output_channel)
         self.FeatureExtraction_output = output_channel
-        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
+        self.AdaptiveAvgPool = nn.AvgPool2d((1, 3))  # Transform final (imgH/16-1) -> 1
+                                                     # Assuming imgH=64
 
         """ Sequence modeling"""
         self.SequenceModeling = nn.Sequential(
